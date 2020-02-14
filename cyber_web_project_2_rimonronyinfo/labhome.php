4a64b2ce2c75027860519c47db0309689192ccb2
Rimon Mahamud Rony <rimonronycste11@gmail.com>
2020-02-15 02:48:23 +0600

    rimonronyinfo

--- cyber_web_project_2_rimonronyinfo/labhome.php
+++ cyber_web_project_2_rimonronyinfo/labhome.php
@@ -10,7 +10,7 @@
       <div class="card" style="width: 18rem;">
           <img src="image/login.jpg" height="200" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Simple LOg in system</h5>
+            <h5 class="card-title" style="color:green;">Simple Log in system</h5>
             <p class="card-text">You may break the log in system and access the system. Or you can find out the vulnerability of the site.</p>
             <a href="https://thesiswebsite12.000webhostapp.com/" class="btn btn-primary">Go to the Log in system</a>
           </div>
@@ -18,11 +18,11 @@
     </div>
     <div class="col-sm">
         <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="...">
+          <img src="image/blood.jpg" height="100" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title" style="color:green;">Blood Bank Management System</h5>
+            <p class="card-text">If you can break the the login system, You can enter the website. But your task is not only break the system, you have to analyze the traffic of the system , database and other system also. </p>
+            <a href="https://thesisbloodwebsite.000webhostapp.com/" class="btn btn-primary">GO TO THE SYSYTEM</a>
           </div>
         </div>
     </div>
